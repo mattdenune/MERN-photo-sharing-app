34077d36e721872324c23e610d0ce7469a399433
Matthew Denune <mattdenune@gmail.com>
2023-07-11 15:28:08 -0400

    added UserPlaces

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-d
 import './App.css';
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 
@@ -15,6 +16,9 @@ function App() {
         <Switch>
           <Route path="/" exact>
             <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces/>
           </Route>
           <Route path="/places/new" exact>
             <NewPlace />
